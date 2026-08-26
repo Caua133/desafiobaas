9f2b2ff84f7b11a9d29b96f88f84c45af9008ed5
Ibsnbrn <tavaresibsonbruno@gmail.com>
2026-08-26 19:50:05 -0300

    Fix(Bug4) Arrumado o Bug de qunado era criado um personagem, ele não aparecia

--- src/services/personagens.ts
+++ src/services/personagens.ts
@@ -16,7 +16,7 @@ import {
 } from "firebase/firestore";
 import { db } from "@/firebase/config";
 import type { Classe, Personagem } from "@/types";
-
+// bah
 // ---------------------------------------------------------------------------
 // LISTAR — BUG 04 🐛
 // ---------------------------------------------------------------------------
